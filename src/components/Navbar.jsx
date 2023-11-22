2810275ad6390d0bfb49455cb3f4483d22092c0d
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-11-22 16:05:47 +0100

    Created Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,19 @@ import Logo from "../assets/images/Mayank (1).png";
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: {
+          sm: "122px",
+          xs: "40px",
+          mt: { sm: "32px", xs: "20px" },
+          justifyContent: "none",
+        },
+      }}
+      px="20px"
+    >
       <Link to="/">
         <img
           src={Logo}
@@ -17,6 +29,27 @@ const Navbar = () => {
           }}
         />
       </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #FF2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+          }}
+        >
+          Exercises
+        </a>
+      </Stack>
     </Stack>
   );
 };
