2db4e81718609c4849be58ee66ec4e231b3a0ffa
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-11-24 19:59:58 +0100

    Designed the HeroBanner front Home page of the website

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Box, Typography, Button, Grid } from "@mui/material";
+import HeroBannerImage from "../assets/images/1banner.png";
+
+const HeroBanner = () => {
+  return (
+    <Grid container spacing={4}>
+      {/* Left side for text content */}
+      <Grid item xs={12} md={6}>
+        <Box
+          sx={{
+            mt: {
+              lg: "212px",
+              xs: "70px",
+            },
+            ml: { sm: "50px" },
+          }}
+          position="relative"
+          p="20px"
+        >
+          <Typography color="#FF2625" fontSize="26px" fontWeight="600">
+            Fitness Akhada
+          </Typography>
+          <Typography
+            fontWeight="700"
+            sx={{
+              fontSize: { lg: "44px", xs: "40px" },
+            }}
+            mb="23px"
+            mt="30px"
+          >
+            Eat, Sleep <br /> Gym, Repeat!
+          </Typography>
+          <Typography fontSize="22px" lineHeight="35px" mb={4}>
+            The most effective exercises are here{" "}
+          </Typography>
+          <Button
+            variant="contained"
+            color="error"
+            href="#exercises"
+            sx={{
+              backgroundColor: "#ff2625",
+              padding: "10px",
+            }}
+          >
+            Explore Exercises
+          </Button>
+          <Typography
+            fontWeight={600}
+            color="#ff2625"
+            sx={{
+              opacity: 0.1,
+              display: {
+                lg: "block",
+                xs: "none",
+              },
+            }}
+            fontSize="200px"
+          >
+            EXERCISE{" "}
+          </Typography>
+        </Box>
+      </Grid>
+
+      {/* Right side for the image */}
+      <Grid item xs={12} md={6}>
+        <img
+          src={HeroBannerImage}
+          alt="banner"
+          className="hero-banner-img"
+          style={{ width: "100%", height: "auto", maxWidth: "400px" }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HeroBanner;
